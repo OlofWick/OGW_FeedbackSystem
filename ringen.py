4f35026924df4d878aa0360e899ee596d25e58a2
OlofWick <olof.wickstrom@gmail.com>
2017-02-13 21:35:24 +0000

    Further testing

--- ringen.py
+++ ringen.py
@@ -1,4 +1,14 @@
 import requests
-r = requests.get("http://lundarallarna.se")
+import json
+
+
+r = requests.get("http://vxlgrp0-1.local")
 print (r.status_code)
+print (r.json())
+print (json.dumps(r.json()))
+test = json.loads(json.dumps(r.json()))
+print (test)
+print (test['Name'])
+
+
 
