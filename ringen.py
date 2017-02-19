98fd16b759802887c4c8f9cac7760bd2e667f268
OlofWick <olof.wickstrom@gmail.com>
2017-02-19 14:38:46 +0100

    Added exception handling

--- ringen.py
+++ ringen.py
@@ -4,12 +4,28 @@ import requests
 import json
 
 def query(name):
-    ip = socket.gethostbyname(name)
+    ip = None
+    while ip is None :
+        try:
+            ip = socket.gethostbyname(name)
+        except (socket.timeout, socket.gaierror, socket.herror) as e:
+            print ("Sensor " + name +  ": IP address lookup error. Retrying...")
+            print (e)
+            time.sleep(30) # Sleep 30s if we couldn't find the host and then try again
+        else:
+            print("Sensor " + name +  " has IP address " + ip)
+            
     while True:
-        r = requests.get("http://" + ip)
-        print (r.status_code)
-        print (r.json())
-        print (r.json()['SW_Version'])
-        time.sleep(30)
-
+        try:
+            r = requests.get("http://" + ip)
+        except Exception as e:
+            print("Sensor " + name + " at IP " + ip + " not available. Retrying")
+            print(e)
+            time.sleep (10) # Sleep 10s and try again
+        else:
+            print (r.status_code)
+            print (r.json())
+            print (r.json()['SW_Version'])
+            time.sleep(15) # Poll every 15s
+        
 query('vxlgrp0-3.local')
