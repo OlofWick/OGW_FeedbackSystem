d4f3db52d249c78c6b90eef4d21144dbfc0c0c44
OlofWick <olof.wickstrom@gmail.com>
2017-02-19 18:47:11 +0100

    Added multithreading

--- ringen.py
+++ ringen.py
@@ -2,30 +2,51 @@ import time
 import socket
 import requests
 import json
+import threading
 
-def query(name):
-    ip = None
-    while ip is None :
-        try:
-            ip = socket.gethostbyname(name)
-        except (socket.timeout, socket.gaierror, socket.herror) as e:
-            print ("Sensor " + name +  ": IP address lookup error. Retrying...")
-            print (e)
-            time.sleep(30) # Sleep 30s if we couldn't find the host and then try again
-        else:
-            print("Sensor " + name +  " has IP address " + ip)
-            
-    while True:
-        try:
-            r = requests.get("http://" + ip)
-        except Exception as e:
-            print("Sensor " + name + " at IP " + ip + " not available. Retrying")
-            print(e)
-            time.sleep (10) # Sleep 10s and try again
-        else:
-            print (r.status_code)
-            print (r.json())
-            print (r.json()['SW_Version'])
-            time.sleep(15) # Poll every 15s
-        
-query('vxlgrp0-3.local')
+class sensorReader (threading.Thread):
+
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.ip = None
+
+    def run(self):
+        while self.ip is None :
+            try:
+                self.ip = socket.gethostbyname(self.name)
+            except (socket.timeout, socket.gaierror, socket.herror) as e:
+                print ("Sensor " + self.name +  ": IP address lookup error. Retrying...")
+                print (e)
+                time.sleep(30) # Sleep 30s if we couldn't find the host and then try again
+            else:
+                print("Sensor " + self.name +  " has IP address " + self.ip)
+
+        while True:
+            try:
+                r = requests.get("http://" + self.ip)
+            except Exception as e:
+                print("Sensor " + self.name + " at IP " + self.ip + " not available. Retrying")
+                print(e)
+                time.sleep (10) # Sleep 10s and try again
+            else:
+                semaphore.acquire()
+                print (r.status_code)
+                print (r.json())
+                print (r.json()['SW_Version'])
+                semaphore.release()
+                time.sleep(15) # Poll every 15s
+
+semaphore = threading.Lock()
+
+grp1Reader = sensorReader(1, 'vxlgrp0-1.local')
+grp3Reader = sensorReader(1, 'vxlgrp0-3.local')
+
+grp1Reader.start()
+grp3Reader.start()
+
+grp1Reader.join()
+grp3Reader.join()
+
+print ("Exiting ringen.py")
